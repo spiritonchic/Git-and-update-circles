bc1b2f554121fbfa086650d73774b46df000151e
spiritonchic <vanya.ershov.05@mail.ru>
2022-11-21 00:13:19 +0300

    First Commit

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 import sys
 from random import randint
-
-from PyQt5 import uic
 from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QPainter, QPen
+from PyQt5.QtGui import QPainter, QPen, QColor
 from PyQt5.QtWidgets import QApplication, QMainWindow
+from UI import Ui_MainWindow
 
-class MyWidget(QMainWindow):
+class MyWidget(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        self.setupUi(self)
         self.flag = False
         self.pushButton.clicked.connect(self.run)
 
@@ -21,7 +20,7 @@ class MyWidget(QMainWindow):
         if self.flag:
             qp = QPainter()
             qp.begin(self)
-            qp.setPen(QPen(Qt.yellow,  8, Qt.SolidLine))
+            qp.setPen(QPen(QColor(randint(0, 255), randint(0, 255), randint(0, 255)),  8, Qt.SolidLine))
             size = randint(10, 200)
             qp.drawEllipse(randint(0, self.width() - size), randint(0, self.height() - size), size, size)
             qp.end()
